bf52ad23af7c6c856e9dc5c8bb58305f7f4cb4a8
Fabio Mendoza Bello <fmendoza@nartexsoft.com>
2020-05-08 22:08:53 +0100

    Comentado y probando modulos, enrutacion y lazy-loading

--- Modulo2/mi-aplicacion/src/app/app.module.ts
+++ Modulo2/mi-aplicacion/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, enableDebugTools } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,6 +6,33 @@ import { AppComponent } from './app.component';
 import { MiComponenteComponent } from './mi-componente/mi-componente.component';
 import { ViewChildComponent } from './view-child/view-child.component';
 import { TestComponent } from './test/test.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {path:'', component:ViewChildComponent},
+  {path:'products/:productId', component: TestComponent},
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: './login/login.module#LoginPageModule'
+  },
+  {
+    path: 'map',
+    loadChildren: './map/map.module#MapPageModule'
+  },
+  {
+    path: 'place-page',
+    loadChildren: './place-page/place-page.module#PlacePageModule'
+  },
+  {
+    path: 'user-stats',
+    loadChildren: './user-stats/user-stats.module#UserStatsPageModule'
+  }
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +43,12 @@ import { TestComponent } from './test/test.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+  //  RouterModule.forChild(...)
+    RouterModule.forRoot(
+      routes,
+      {enableTracing: true, useHash: true}
+      )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Modulo2/mi-aplicacion/src/app/test/test.component.ts
+++ Modulo2/mi-aplicacion/src/app/test/test.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush  
 })
 export class TestComponent implements OnInit {
 
-  constructor(private changeDetector: ChangeDetectorRef) { }
+  constructor(private changeDetector: ChangeDetectorRef, private router: Router, route: ActivatedRoute) {
+    const id: string = route.snapshot.params.id;
+    const url: string = route.snapshot.url.join('');
+    const user = route.snapshot.data.user;
+   }
 
   ngOnInit(): void {
     /* Tarea computacioalmente costosa que guarda relación con la vista*/
     this.changeDetector.detectChanges(); 
+
+    //this.router.navigateByUrl("/login");
+
+    // this.router.paramMap.subscribe(params => {
+    //   this.product = products[+params.get('productId')];
+    // });
+
+
   }
 
 }
