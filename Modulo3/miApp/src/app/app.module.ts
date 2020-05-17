de9bbd0e6dde630d9c07455364bc881a570e4f04
Fabio Mendoza Bello <fmendoza@nartexsoft.com>
2020-05-17 20:19:24 +0100

    Actualizado import pipe

--- Modulo3/miApp/src/app/app.module.ts
+++ Modulo3/miApp/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ImgAsyncLoaderDirective } from './directive/img-async-loader.directive';
 import { TestDirectivaComponent } from './test-directiva/test-directiva.component';
-import { TruncateTextPipe } from './truncate-text.pipe';
+import { TruncateTextPipe } from './pipe/truncate-text.pipe';
 import { TestPipeComponent } from './test-pipe/test-pipe.component';
 
 @NgModule({
