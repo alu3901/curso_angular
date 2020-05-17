cf5e21207dcb09e5432b6af213c76b67a413e20d
Fabio Mendoza Bello <fmendoza@nartexsoft.com>
2020-05-17 20:17:10 +0100

    Uso de pipes

--- Modulo3/miApp/src/app/app.module.ts
+++ Modulo3/miApp/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ImgAsyncLoaderDirective } from './directive/img-async-loader.directive';
 import { TestDirectivaComponent } from './test-directiva/test-directiva.component';
+import { TruncateTextPipe } from './truncate-text.pipe';
+import { TestPipeComponent } from './test-pipe/test-pipe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ImgAsyncLoaderDirective,
-    TestDirectivaComponent
+    TestDirectivaComponent,
+    TruncateTextPipe,
+    TestPipeComponent
   ],
   imports: [
     BrowserModule
